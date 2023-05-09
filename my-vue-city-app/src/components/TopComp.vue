f535ab6ab8d5efe60f4aad252d28312d8d738575
tztx2004 <tztx2004@gmail.com>
2023-05-09 14:32:48 +0900

    230509 오전

--- my-vue-city-app/src/components/TopComp.vue
+++ my-vue-city-app/src/components/TopComp.vue
@@ -8,17 +8,26 @@
       따라서 리액티브 데이터를 직접 해당자리에 사용해야한다! -->
         <!-- <li v-for="(v,i) in sdata" v-bind:key="i" v-if="i!=='인트로'"> -->
         <li v-for="(v,i) in this.$store.state.cityData" v-bind:key="i" v-if="i!=='인트로'">
-            <a href="#" v-on:click="chgData(i)">{{ i }}</a>
+            <a href="#" v-on:click.prevent="chgData(i)">{{ i }}</a>
         </li>
     </ul>
     <!-- 메뉴선택이동링크 -->
     <div class="m2">
-      <a href="#" v-on:click="chgMenu(num)" v-text="'메뉴'+num"></a>
+      <!--
+        v-on:이벤트명.prevent
+        기본기능막기 옵션 .prevent는 event.preventDefault()와 같다!
+        참고) 
+        v-on:이벤트명.stop
+        이벤트버블링막기 옵션 .stop는 event.stopPropagation와 같다!
+      -->
+      <a href="#" v-on:click.prevent="chgMenu(num)" v-text="'메뉴'+num"></a>
     </div>
   </header>
 </template>
 
 <script>
+// 제이쿼리 임포트하기!
+import $ from "jquery"
 export default {
   name: 'TopArea',
   data () {
@@ -44,7 +53,40 @@ export default {
       // 메뉴1/메뉴2 전환을 위한 변수변경하기
       // 컴포넌트 변수인 num을 변경한다!
       n===1? this.num =2: this.num=1;
-    }
+
+      // 메뉴변경시 DOM이 변경되므로
+      // 제이쿼리 메서드 호출하기!
+      // 단, 제이쿼리 코드블럭으로 싸서
+      // 호출함으로 DOM로드 후 실행보장!
+      $(()=>this.setJQ());
+    },
+    // 제이쿼리 셋팅 메서드
+    setJQ(){
+      // 링크 클릭시 a에 클래스 on주기
+      $(".gnb a").click(function(){
+          $(this).addClass("on")
+          .parent().siblings()
+          .find("a").removeClass("on");
+
+          // 박스 애니
+          showBx();
+      }); ////////// click ///////////
+
+      function showBx(){
+          // 이미지와 설명박스 순서대로 나타나기
+          $("main img")
+          .css({opacity:0}).stop()
+          .delay(500).fadeTo(500,1);
+
+          $("main p")
+          .css({opacity:0}).stop()
+          .delay(1000).fadeTo(500,1);
+      } ////// showBx ////
+    },
+  },
+  // DOM을 만들고 난후
+  mounted(){
+    this.setJQ();
   }
 }
 </script>
